5c3af1933cf7fba625a819574eff740ae568f643
Indrajeet <indrajeetkumarnayak3@gmail.com>
2023-10-19 19:53:56 +0530

    code updated

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -3,9 +3,9 @@ import  ProductModel from "../models/product_model.js"
 export default class productController {
   getProducts(req, res) {
     let products = ProductModel.get();
-    console.log(products)
-    // console.log(path.resolve());
 
-    return res.sendFile(path.join(path.resolve(), "src",'views', "products.html"));
+
+    // console.log(products)
+      res.render("products.ejs",{products: products})
   }
 }
