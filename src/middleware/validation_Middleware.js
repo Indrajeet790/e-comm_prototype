84a968ea1aceff5802499ac0819e18a5e42d89f4
Indrajeet <indrajeetkumarnayak3@gmail.com>
2023-10-20 16:26:40 +0530

    Using Express Validator for data

--- src/middleware/validation_Middleware.js
+++ src/middleware/validation_Middleware.js
@@ -1,4 +1,4 @@
-const validateRequest = (req,res,next)=>{
+/*const validateRequest = (req,res,next)=>{
     const { name, price, imageUrl } = req.body;
     let errors = [];
     if (!name || name.trim() == '') {
@@ -24,4 +24,51 @@ const validateRequest = (req,res,next)=>{
 };
 
 export default validateRequest;
-    
+ */
+
+import {
+    body,
+    validationResult,
+  } from 'express-validator';
+  
+  const validateRequest = async (
+    req,
+    res,
+    next
+  ) => {
+    console.log(req.body);
+    // 1. Setup rules for validation.
+    const rules = [
+      body('name')
+        .notEmpty()
+        .withMessage('Name is required'),
+      body('price')
+        .isFloat({ gt: 0 })
+        .withMessage(
+          'Price should be a positive value'
+        ),
+      body('imageUrl')
+        .isURL()
+        .withMessage('Invalid url'),
+    ];
+  
+    // 2. run those rules.
+    await Promise.all(
+      rules.map((rule) => rule.run(req))
+    );
+  
+    // 3. check if there are any errors after running the rules.
+    var validationErrors = validationResult(req);
+    console.log(validationErrors);
+    // 4. if errors, return the error message
+    if (!validationErrors.isEmpty()) {
+      return res.render('new_product', {
+        errorMessage:
+          validationErrors.array()[0].msg,
+      });
+    }
+    next();
+  };
+  
+  export default validateRequest;
+  
