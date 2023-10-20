b7cdec98a045cc1d52eb1c52cb2393fa70caeb65
Indrajeet <indrajeetkumarnayak3@gmail.com>
2023-10-20 13:32:39 +0530

    Validation Middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path';
 import ProductController from "./src/controllers/product_controller.js";
 import expressLayouts from 'express-ejs-layouts';
+import validationMiddleware from './src/middleware/validation_Middleware.js'
 const server = express();
 
 // parse form data
@@ -20,7 +21,7 @@ const productController = new ProductController()
 
 server.get('/',productController.getProducts)
 server.get('/new',productController.getAddProduct)
-server.post('/',productController.postAddProduct)
+server.post('/',validationMiddleware,productController.postAddProduct)
 
 server.use(express.static("src/views"));
 server.listen(8000,(err)=>{
--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -14,33 +14,13 @@ getAddProduct(req, res, next) {
     errorMessage: null,
   });
 }
-postAddProduct(req,res){
+postAddProduct(req,res,next){
    // validate data
-   const { name, price, imageUrl } = req.body;
-   let errors = [];
-   if (!name || name.trim() == '') {
-     errors.push('Name is required');
-   }
-   if (!price || parseFloat(price) < 1) {
-     errors.push(
-       'Price must be a positive value'
-     );
-   }
-   try {
-     const validUrl = new URL(imageUrl);
-   } catch (err) {
-     errors.push('URL is invalid');
-   }
-
-   if (errors.length > 0) {
-     return res.render('new_product', {
-       errorMessage: errors[0],
-     });
-   }
+  
   // access data from form.
-  console.log(req.body);
+  // console.log(req.body);
   ProductModel.add(req.body);
-  let products = ProductModel.get();
+  var products = ProductModel.get();
   return res.render('products', {products});
 }
 
